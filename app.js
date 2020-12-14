73d3a840321decd4dce50fbc226cdcc0054c3985
bassyu <yrkdgus@gmail.com>
2020-12-14 18:14:43 +0900

    Add CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routes/user');
 const postRouter = require('./routes/post');
 const db = require('./models');
@@ -11,6 +12,10 @@ db.sequelize.sync()
   })
   .catch(console.error);
 
+app.use(cors({
+  origin: '*',
+  // credentials: false,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
